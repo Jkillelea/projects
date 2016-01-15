318faef2a05419ceefcefa36e6234b8c1bd97c8c
Jacob Killelea <Jkillelea@protonmail.ch>
2016-01-14 18:55:28 -0700

    new things from codeacademy exercises

--- rubies/histogram.rb
+++ rubies/histogram.rb
@@ -9,3 +9,10 @@ frequencies = Hash.new(0)
 words.each do |word| #Each time we find a word, increment it's value in the iterator by 1
   frequencies[word] += 1
 end
+
+frequencies = frequencies.sort_by { |word|  }
+frequencies.reverse!
+
+frequencies.each do |word, count|
+  puts "#{word}" + " " + "#{count}"
+end
