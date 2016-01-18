69cde6e38a6c41ffb335aa8db795462d2abfffb4
Jacob Killelea <Jkillelea@protonmail.ch>
2016-01-18 00:45:35 -0700

    updates to code, more scripts

--- javagamethingy/drawingComponent.java
+++ javagamethingy/drawingComponent.java
@@ -1,11 +1,15 @@
-import java.awt.JComponent;
+import javax.swing.JComponent;
 import java.awt.Graphics;
-
+import java.awt.Graphics2D;
+import java.awt.Rectangle;
 
 public class drawingComponent extends JComponent {
 
   public void paintComponent(Graphics g){
-    super.paintComponent(Graphics g);
-  }
+    Graphics2D g2 = (Graphics2D) g;
 
+    Rectangle rect1 = new Rectangle(5, 5, 100, 200); //x, y, width, height
+
+    g2.draw(rect1); //draw the Rectangle.
+  }
 }
--- javagamethingy/main.java
+++ javagamethingy/main.java
@@ -2,9 +2,9 @@ import javax.swing.JFrame;
 import java.awt.Rectangle;
 
 
-public class Main{
+public class main{
 
-  public Main(){
+  public main(){
     System.out.println("You're not supposed to create instances of this class!");
   }
 
@@ -14,13 +14,12 @@ public class Main{
 
     JFrame window = new JFrame(); //create the window
     window.setSize(640,480); //x,y
-    window.setTitle(args[0]); //name it whatever we put in for arg 0
+    window.setTitle("window"); //name it whatever we put in for arg 0
     window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     window.setVisible(true);
 
-    Rectangle rectangle = new Rectangle(5, 10, 300, 100); //x, y, width, height
-
-
+    drawingComponent DC = new drawingComponent();
+    window.add(DC);
 
   }
 
