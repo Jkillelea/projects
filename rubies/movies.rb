1a3a77efa3b87387a510045637c23326338ec3d7
Jacob Killelea <Jkillelea@protonmail.ch>
2016-01-18 01:06:15 -0700

    more things, courtesy of codeacademy

--- rubies/movies.rb
+++ rubies/movies.rb
@@ -1,18 +1,49 @@
 movies = {
-    favorite: "Dr. Strangelove"
+   "Dr. Strangelove" => 5
 }
 
-puts "So, what's your favorite movie?"
+puts "add"
+puts "update"
+puts "display"
+puts "delete"
+puts "Select an option: "
 
 choice = gets.chomp
 
 case choice
 
 when "add"
-puts "Added!"
+  puts "What is the movie's title?"
+  title = gets.chomp
+
+  puts "What rating?"
+  rating = gets.chomp.to_i
+
+  if movies[title] = nil
+      movies[title] = rating
+  else
+      puts "this movie has already been entered!"
+  end
+
 
 when "update"
-puts "Updated!"
+   puts "Which movie to update?"
+   movies.each do |title, rating|
+     puts title + " : " + rating.to_s
+   end
+
+   to_update = gets.chomp.to_s
+
+   if movies[to_update] = nil
+     puts "That movie isn't in the system!"
+   else
+     print "enter a new rating : "
+     new_rating = gets.chomp
+
+     movies[to_update] = new_rating.to_i
+   end
+
+
 
 when "display"
 puts "Movies!"
