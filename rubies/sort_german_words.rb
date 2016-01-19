a2cbee05af880986ed00143f517ad95899339d5b
Jacob Killelea <Jkillelea@protonmail.ch>
2016-01-18 19:03:47 -0700

    wanwted to alphabetize a list - ruby makes it easy.

---
+++ rubies/sort_german_words.rb
@@ -0,0 +1,44 @@
+words = [
+  "anfangen",
+  "beginnen",
+  "haben",
+  "lesen",
+  "sein",
+  "verstehen",
+  "helfen",
+  "aussteigen",
+  "bekommen",
+  "werden",
+  "nehmen",
+  "sprechen",
+  "ziehen",
+  "laufen",
+  "einladen",
+  "fahren",
+  "heissen",
+  "schlafen",
+  "stehen",
+  "bleiben",
+  "sitzen",
+  "mitbringen",
+  "finden",
+  "kommen",
+  "schreiben",
+  "wissen",
+  "essen",
+  "sterben",
+  "geben",
+  "gehen",
+  "liegen",
+  "sehen",
+  "trinken",
+  "denken",
+  "vergessen",
+]
+
+
+words.sort!
+
+words.each do |word|
+  puts word
+end
