6837758ec807361af1dc5a719ab824d498b7577b
Jacob Killelea <Jkillelea@protonmail.ch>
2016-01-22 20:01:53 -0700

    Create intlength - a class for determining the length of an int

---
+++ rubies/intlength.rb
@@ -0,0 +1,28 @@
+class IntLengthCounter
+##returns the length of an int supplied sent through len
+@length #instance variable for length
+
+  def initialize
+    @length = 0
+  end
+
+  private #RECURSIVE FUNCTION - @length is incremented for every power of 10 in the passed number
+  def length(int)
+    unless int == 0
+      @length += 1
+      length(int / 10)
+    end
+  end
+
+  public #Public interface for length - keeps things nice and straightforwards
+  def len(num)
+    @length = 0
+    length(num)
+    return @length
+  end
+
+end
+
+counter = IntLengthCounter.new
+
+puts counter.len(gets.chomp.to_i).to_s
