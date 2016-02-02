4034dc8834e90e022eb5e27a1df51bcd5414a835
Jacob Killelea <Jkillelea@protonmail.ch>
2016-02-01 20:42:28 -0700

    mathextention docs updated

--- rubies/mathextention.rb
+++ rubies/mathextention.rb
@@ -10,6 +10,7 @@ module MathExtention
   end
 
  # Returns the number of digits of an int or fixnum, not the logBaseTen value: 10**3 = 1000 and returns 4, not 3
+ # NOTE that for very large floats in the form x.xxxxx e+xxx, this function will evaluate the length of the string, not the actual length of the number
   public #Public interface for length - keeps things nice and straightforwards
   def len(num)
     return num.to_s.length
