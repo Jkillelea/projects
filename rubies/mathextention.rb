01def2730118f74084b95fbc316be475c5ec829e
Jacob Killelea <Jkillelea@protonmail.ch>
2016-02-14 01:00:18 -0700

    cosmetic changes

--- rubies/mathextention.rb
+++ rubies/mathextention.rb
@@ -1,9 +1,7 @@
 module MathExtention
 
-  # Needed to remove nomethod errors
-  extend self
 
-  def pythagorean(*args)
+  def self.pythagorean(*args)
     square_sum = 0
     args.each { |arg| square_sum = square_sum + arg**2}
     return square_sum**(0.5)
@@ -12,8 +10,13 @@ module MathExtention
  # Returns the number of digits of an int or fixnum, not the logBaseTen value: 10**3 = 1000 and returns 4, not 3
  # NOTE that for very large floats in the form x.xxxxx e+xxx, this function will evaluate the length of the string, not the actual length of the number
   public #Public interface for length - keeps things nice and straightforwards
-  def len(num)
+  def self.len(num)
     return num.to_s.length
+  end
+
+  def self.multiply(*args)
+    args.each { |x| product = product * x  }
+    return product
   end
 
 
