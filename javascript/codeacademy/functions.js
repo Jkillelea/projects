a6ed0891f38d3a437d0dc13c4e01cc28fd043248
Jacob Killelea <Jkillelea@protonmail.ch>
2016-03-09 12:41:36 -0700

    eigth exercise

--- javascript/codeacademy/functions.js
+++ javascript/codeacademy/functions.js
@@ -1,7 +1,5 @@
-// Write your function starting on line 3
-
-function perimeterBox(length, width) {
-  return length*2 + width*2;
+var nameString = function(name) {
+	return "Hi, I am " + name;
 };
 
-perimeterBox(20, 1)
+console.log(nameString("Bin Laden"));
