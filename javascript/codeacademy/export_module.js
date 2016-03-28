76eae5239d48bfcebdfa047af347b3fbdbc25ff7
Jacob Killelea <Jkillelea@protonmail.ch>
2016-03-27 21:38:21 -0600

    had a file moved into it

--- javascript/codeacademy/export_module.js
+++ javascript/codeacademy/export_module.js
@@ -1,11 +1,8 @@
 module.exports = {
-
   exported_function: function (random_thing) {
     console.log("TOP KEK");
   }
-
   sample_string: "potato";
-
 };
 
 module.exports.another_string = "this could be a function too";
