0bbb9696a2f436a7af49e3df350cbb12757f01e5
Jacob Killelea <jkillelea@protonmail.ch>
2016-06-03 12:28:40 -0700

    includes a little exception handling now.

--- rubies/metar_reader.rb
+++ rubies/metar_reader.rb
@@ -4,18 +4,22 @@ require "open-uri"
 require "nokogiri"
 
 # airport uses ICAO identifier
-airport = "KBDU"
+airport = "KPAO"
 dataSource = "metars"
 format = "xml"
 hoursBeforeNow = 1
 
 
-# get and translate into XML
-metar = Nokogiri::XML(open("https://www.aviationweather.gov/adds/dataserver_current/httpparam?dataSource=#{dataSource}&requestType=retrieve&format=#{format}&stationString=#{airport}&hoursBeforeNow=#{hoursBeforeNow}"))
+begin
+  # get and translate into XML
+  metar = Nokogiri::XML(open("https://www.aviationweather.gov/adds/dataserver_current/httpparam?dataSource=#{dataSource}&requestType=retrieve&format=#{format}&stationString=#{airport}&hoursBeforeNow=#{hoursBeforeNow}"))
 
-# # get the latest brief
-# metar.search("METAR").each do |e|
-#   puts e
-# end
+  # # get the latest brief
+  # metar.search("METAR").each do |e|
+  #   puts e
+  # end
+  puts metar.search("METAR raw_text").first
 
-puts metar.search("METAR raw_text").first
+rescue
+  puts "unable to load XML data"
+end
