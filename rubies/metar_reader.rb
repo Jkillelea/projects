08bfc5e8998b6d88615a3cdfb297b75512e5c211
Jacob Killelea <Jkillelea@protonmail.ch>
2016-06-13 08:19:46 -0700

    metar reader is now a proper function blocky thingy

--- rubies/metar_reader.rb
+++ rubies/metar_reader.rb
@@ -4,18 +4,22 @@ require "open-uri"
 require "nokogiri"
 
 # airport uses ICAO identifier
-airport = "KBDU"
-dataSource = "metars"
-format = "xml"
+airport        = "KPAO"
+dataSource     = "metars"
+format         = "xml"
 hoursBeforeNow = 1
 
 
-# get and translate into XML
-metar = Nokogiri::XML(open("https://www.aviationweather.gov/adds/dataserver_current/httpparam?dataSource=#{dataSource}&requestType=retrieve&format=#{format}&stationString=#{airport}&hoursBeforeNow=#{hoursBeforeNow}"))
+def get_metar(airport, dataSource, format, hoursBeforeNow)
+  # get and translate into XML
+  metar = Nokogiri::XML(open("https://www.aviationweather.gov/adds/dataserver_current/httpparam?dataSource=#{dataSource}&requestType=retrieve&format=#{format}&stationString=#{airport.upcase}&hoursBeforeNow=#{hoursBeforeNow}"))
 
-# # get the latest brief
-# metar.search("METAR").each do |e|
-#   puts e
-# end
+  if block_given?
+    yield metar
+  end
+end
 
-puts metar.search("METAR raw_text").first
+
+get_metar(airport, dataSource, format, hoursBeforeNow) do |metar|
+  puts metar.search("METAR raw_text").first
+end
