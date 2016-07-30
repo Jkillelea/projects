bedd848e2f951bf2d3adfcc65604f4f669ac8b0e
Jacob Killelea <jkillelea@protonmail.ch>
2016-07-30 01:27:36 -0700

    reworked for clarity

--- c++/misc/structs/struct_test.cpp
+++ c++/misc/structs/struct_test.cpp
@@ -5,22 +5,27 @@ using namespace std;
 struct potato {
   string name;
   int age;
-} tuber;
+} tuber; // first way of creating a variable from a struct
 
-void sayTuber(potato &obj);
+potato russett; // second way of creating a variable from a struct
+
+void sayTuber(potato potato);
 
 int main(int argc, char const *argv[]) {
-
 
   tuber.name = "tuber";
   tuber.age = 1;
 
+  russett.name = "russett";
+  russett.age = 1000000;
+
   sayTuber(tuber);
+  sayTuber(russett);
 
   return 0;
 }
 
-void sayTuber(potato &obj){
-  std::cout << tuber.name << std::endl;
-  std::cout << tuber.age << std::endl;
+void sayTuber(potato potato){
+  std::cout << potato.name << std::endl;
+  std::cout << potato.age << std::endl;
 }
