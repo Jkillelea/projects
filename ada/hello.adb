3e3e0d79424aa5e85106b7fa282b300b8deec8bd
Jacob Killelea <Jkillelea@protonmail.ch>
2016-10-02 22:15:35 -0600

    Revert "to revert"
    This reverts commit 1d3626af53d6cd69c1cc6a503bef560cc7163b1f.

--- ada/hello.adb
+++ ada/hello.adb
@@ -1,7 +1,7 @@
 with Ada.Text_IO; use Ada.Text_IO;
 procedure hello is
 
-  procedure subproc (S : in String) is
+  procedure subproc (S : String) is
   begin -- subproc
     put(S);
   end subproc;
