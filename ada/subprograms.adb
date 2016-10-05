a3ee28594fbc56ae5612797ec20fdde44c4116b5
Jacob Killelea <Jkillelea@protonmail.ch>
2016-10-04 22:50:57 -0600

    messting around

--- ada/subprograms.adb
+++ ada/subprograms.adb
@@ -1,9 +1,10 @@
 --Procedures are basically void functions. They can modify variables passed by reference sometimes.
-with Ada.text_io, Ada.integer_text_io;
-use  Ada.text_io, Ada.integer_text_io; -- note that these names are NOT case-sensitive!
+with Ada.Text_IO, Ada.Integer_Text_IO;
+use  Ada.Text_IO, Ada.Integer_Text_IO; -- note that these names are NOT case-sensitive!
 
 procedure subprograms is
-  package io renames Ada.Text_IO;
+--  package IO    renames Ada.Text_IO;
+--  package IT_IO renames Ada.Integer_Text_IO;
 
   procedure A_Test (A, B: in Integer; C: out Integer) is -- subprocedure
   begin
@@ -26,7 +27,8 @@ procedure subprograms is
 
 begin
   A_Test(x, y, z); -- x, y are input-only. z is output-only, and modified similar to a pass-by-reference function, a-la C
-  put(z);          -- default column width is 11 for some reason
+  put(z, 1);          -- default column width is 11 for some reason
   new_line;
-  put(minimum(x, z));
+  z := minimum(x, z);
+  put(z, 1);
 end subprograms;
