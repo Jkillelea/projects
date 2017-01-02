c5fefb28571128763d28a1930ec0fc2038227320
Jacob Killelea <Jkillelea@protonmail.ch>
2017-01-02 02:09:13 -0800

    gtk

--- rubies/gtk_tests/gtk_hello_world_sample.rb
+++ rubies/gtk_tests/gtk_hello_world_sample.rb
@@ -1,30 +1,37 @@
 #!/usr/bin/env ruby
-
 =begin
 sample program with ruby gtk3
 =end
-
 require "gtk3"
 
-button = Gtk::Button.new("Test Button")
-button.signal_connect("clicked") do #these signals are specific to their objects. replacing 'clicked' with 'potato,' for example, will not work
-  puts "Clicked!"
+Thread.new do
+  window = Gtk::Window.new("test")
+  button = Gtk::Button.new(:label => "test button",
+                           :use_underline => true,
+                           :stock_id      => 0)
+
+  button.signal_connect("clicked") do # these signals are specific to their objects. replacing 'clicked' with 'potato,' for example, will not work
+    puts "Clicked!"
+  end
+
+  window.signal_connect("delete_event") do
+    puts "delete occurred"
+  end
+
+  window.signal_connect("destroy") do
+    puts "destroy event occurred"
+    Gtk.main_quit
+  end
+
+  window.border_width = 50
+  window.add(button)
+  window.show_all
+
+  Gtk.main
 end
 
-window = Gtk::Window.new
-window.signal_connect("delete_event") do
-  puts "delete occurred"
-  false
-end
-
-
-window.signal_connect("destroy") do
-  puts "destroy event occurred"
-  Gtk.main_quit
-end
-
-window.border_width = 50
-window.add(button)
-window.show_all
-
-Gtk.main
+Thread.list.each { |t|
+  unless t == Thread.main
+    t.join
+  end
+}
