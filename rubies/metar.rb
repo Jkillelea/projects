a4a38e9fb8382ddcaae314f749d1c3f4313ce0d3
Jacob Killelea <jkillelea@protonmail.ch>
2017-01-04 21:23:04 -0800

    accept blocks in request()

--- rubies/metar.rb
+++ rubies/metar.rb
@@ -15,7 +15,25 @@ class Metar
     @airport          = options[:airport]
     @hours_before_now = options[:hours_before_now]
 
-    # validate input
+    validate_inputs
+
+    @query_string = "https://www.aviationweather.gov/adds/dataserver_current/httpparam?dataSource=#{data_source}&requestType=retrieve&format=#{format}&stationString=#{airport.upcase}&hoursBeforeNow=#{hours_before_now}"
+  end
+
+  def request(&block)
+    response = open(@query_string) # => StringIO object (use object.gets to read the underlying string)
+    if block_given?
+      yield response
+    else
+      return response
+    end
+  end
+
+  private
+  def validate_inputs
+    if @airport.length < 3
+      raise "[ERROR]: Need to be given a 3-letter (FAA) or 4-letter (ICAO) airport code!"
+    end
     if @airport.length < 4
       STDERR.puts "[NOTE]: Airport identifier was less than 4 characters long. Prepending a 'K' (ICAO CONUS) to the airport identifier..."
       @airport = "K#{@airport}"
@@ -29,12 +47,6 @@ class Metar
     if @hours_before_now < 1
       raise "[ERROR]: You must request data at least 1 hour old. You tried #{@hours_before_now} hour(s)."
     end
-
-    @query_string = "https://www.aviationweather.gov/adds/dataserver_current/httpparam?dataSource=#{data_source}&requestType=retrieve&format=#{format}&stationString=#{airport.upcase}&hoursBeforeNow=#{hours_before_now}"
-  end
-
-  def request
-    open(@query_string) # => StringIO object (use object.gets to read the underlying string)
   end
 
 end
