a93acaaf22918a0f8b0b8d4099473d55a2487de6
Jacob Killelea <jkillelea@protonmail.ch>
2017-01-04 21:23:16 -0800

    use CSS selectors

--- rubies/metar_reader.rb
+++ rubies/metar_reader.rb
@@ -6,32 +6,6 @@
 require "nokogiri"
 require_relative './metar.rb'
 
-# def get_metar(args)
-#   # get and translate into XML
-#   begin
-#     url = "https://www.aviationweather.gov/adds/dataserver_current/httpparam?dataSource=#{args[:dataSource]}
-#                                                                             &requestType=retrieve&format=#{args[:format]}
-#                                                                             &stationString=#{args[:airport].upcase}
-#                                                                             &hoursBeforeNow=#{args[:hoursBeforeNow]}"
-#
-#     for str in ["\n", "\t", " "] # clean up into one long string
-#       url.delete!(str)
-#     end
-#     xml   = open(url)          # make request
-#     metar = Nokogiri::XML(xml) # translate
-#
-#   rescue SocketError
-#     STDERR.puts "[ERROR] Failed to connect to internet."
-#     exit 1
-#   end
-#
-#   if block_given?
-#     yield metar
-#   else
-#     return metar
-#   end
-# end
-
 # airport uses ICAO identifier
 if ARGV.empty? # no args given?
   airport = "ksfo"
@@ -50,8 +24,7 @@ args = {
 m = Metar.new args
 metar = Nokogiri::XML(m.request)
 
-# metar = get_metar(args)
-data  = metar.children.search('METAR').first
+data  = metar.css('METAR').first # using css selectors on XML
 
 begin
   data.children.each { |child|
