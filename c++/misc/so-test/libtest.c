08d778424dba0b21c7e2ddaaa4ff4d21d51df6ed
Jacob Killelea <jkillelea@protonmail.ch>
2017-01-11 00:05:06 -0800

    shared library

---
+++ c++/misc/so-test/libtest.c
@@ -0,0 +1,5 @@
+// #include <iostream>
+#include <stdio.h>
+void testFunc() {
+  fprintf(stdout, "Hello!\n");
+}
---
+++ c++/misc/so-test/main.c
@@ -0,0 +1,8 @@
+#include "libtest.h"
+// Need to call the following in bash:
+// $ export LD_LIBRARY_PATH=.
+
+int main(int argc, char const *argv[]) {
+  testFunc();
+  return 0;
+}
