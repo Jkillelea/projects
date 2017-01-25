d9ff10cdeff6547fbc0ed368f6cc9733b7fd4d13
Jacob Killelea <Jkillelea@protonmail.ch>
2017-01-24 22:29:22 -0700

    misc changes

--- ada/more-ada/main.adb
+++ ada/more-ada/main.adb
@@ -3,15 +3,11 @@ use Ada.Text_IO, Persons;
 
 procedure Main is
   package US renames Ada.Strings.Unbounded;
-  type Person_ref is access Person;
 
   Age  : Integer     := 20;
   Name : Person_Name := US.To_Unbounded_String("Jake");
   Me   : Person_ref  := new Person'(Name, Age);
 
-  -- need to create a deallocation procedure for this kind of object
-  procedure Free is new Ada.Unchecked_Deallocation(Object => Person,
-                                                          Name => Person_ref);
 begin
   Put_Line("Age: " & Integer'Image(Me.Age));
   Put_Line("Name: " & US.To_String(Me.Name));
--- ada/more-ada/persons.ads
+++ ada/more-ada/persons.ads
@@ -1,3 +1,4 @@
+with Ada.Unchecked_Deallocation;
 with Ada.Strings.Unbounded; use Ada.Strings.Unbounded;
 
 package Persons is
@@ -7,7 +8,11 @@ package Persons is
     Age  : Integer;
   end record;
 
+  type Person_ref is access Person;
+
   function  Create(Name : Person_Name; Age : Integer) return Person;
+  procedure Free is new Ada.Unchecked_Deallocation(Object => Person,
+                                                    Name  => Person_ref);
   procedure Put_Name(This : Person'Class);
   procedure Put_Age(This : Person'Class);
 end Persons;
