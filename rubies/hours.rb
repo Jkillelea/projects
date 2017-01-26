15f95819024dc7df68e10c81e27e29bd012b085e
Jacob Killelea <Jkillelea@protonmail.ch>
2017-01-25 20:53:06 -0700

    ensure that hours.yaml is created in user home

--- rubies/hours.rb
+++ rubies/hours.rb
@@ -1,7 +1,7 @@
 # A little script to write the number of hours worked on a day to a yaml file for reference
 require "yaml"
 
-HOURS_FILE = '~/hours.yaml'
+HOURS_FILE = "#{Dir.home}/hours.yaml"
 TODAY      = "#{Time.now.month}/#{Time.now.day}/#{Time.now.year}"
 
 if File.exists? HOURS_FILE
@@ -13,6 +13,7 @@ if File.exists? HOURS_FILE
     hours = Hash.new
   end
 else
+  file = File.new HOURS_FILE, 'a+'
   hours = Hash.new
 end
 
@@ -22,17 +23,21 @@ start_time = gets.chomp
 puts "End time?"
 end_time = gets.chomp
 
+puts "Summary?"
+summary = gets.chomp
+
 if hours[TODAY] != nil # if there's already an entry from today, append the new one
   hours[TODAY] = { 'previously today' => hours[TODAY],
-                   'start' => start_time,
-                   'end'   => end_time}
+                   'start'            => start_time,
+                   'end'              => end_time,
+                   'summary'          => summary}
 else
-  hours[TODAY] = { 'start' => start_time,
-                   'end'   => end_time}
+  hours[TODAY] = { 'start'   => start_time,
+                   'end'     => end_time,
+                   'summary' => summary}
 end
 
 # rewrite file with new data
 file  = File.open HOURS_FILE, 'w+'
 file.write YAML.dump hours
 file.close if not file.closed?
-
