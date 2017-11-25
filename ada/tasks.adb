f7671c275f6dc254a58b2de4dd17f875e56510d0
Jacob Killelea <Jkillelea@protonmail.ch>
2017-11-25 13:27:25 -0700

    a simple example of multithreading using Ada tasks from http://blog.adacore.com/theres-a-mini-rtos-in-my-language

---
+++ ada/tasks.adb
@@ -0,0 +1,29 @@
+with Ada.Text_IO;   use Ada.Text_IO;
+with Ada.Real_Time; use Ada.Real_Time;
+
+procedure Tasks is
+
+    task My_Task  -- pre declare
+      with Priority => 0; -- higher priority (max 256)
+
+    task body My_Task is
+      Wait : constant Time_Span := Milliseconds (100);
+      Next_Time : Time := clock; -- now
+    begin
+      loop
+        Put_Line ("Task");
+        Next_Time := Next_Time + Wait;
+        delay until Next_Time;
+      end loop;
+    end My_Task;
+
+    Wait : constant Time_Span := Milliseconds (200);
+    Next_Time : Time := clock; -- now
+begin
+  loop
+    Put_Line ("Main Loop");
+    Next_Time := Next_Time + Wait;
+    delay until Next_Time;
+  end loop;
+end Tasks;
+
