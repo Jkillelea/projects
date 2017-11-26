94ac9087196b2c1a381a8d97c93ab81053f67abf
Jacob Killelea <Jkillelea@protonmail.ch>
2017-11-25 21:57:56 -0700

    formatting

--- ada/tasks.adb
+++ ada/tasks.adb
@@ -3,22 +3,22 @@ with Ada.Real_Time; use Ada.Real_Time;
 
 procedure Tasks is
 
-    task My_Task  -- pre declare
-      with Priority => 0; -- higher priority (max 256)
+  task My_Task  -- pre declare
+    with Priority => 0; -- higher priority (max 256)
 
-    task body My_Task is
-      Wait : constant Time_Span := Milliseconds (100);
-      Next_Time : Time := clock; -- now
-    begin
-      loop
-        Put_Line ("Task");
-        Next_Time := Next_Time + Wait;
-        delay until Next_Time;
-      end loop;
-    end My_Task;
-
-    Wait : constant Time_Span := Milliseconds (200);
+  task body My_Task is
+    Wait : constant Time_Span := Milliseconds (100);
     Next_Time : Time := clock; -- now
+  begin
+    loop
+      Put_Line ("Task");
+      Next_Time := Next_Time + Wait;
+      delay until Next_Time;
+    end loop;
+  end My_Task;
+
+  Wait : constant Time_Span := Milliseconds (200);
+  Next_Time : Time := clock; -- now
 begin
   loop
     Put_Line ("Main Loop");
@@ -26,4 +26,3 @@ begin
     delay until Next_Time;
   end loop;
 end Tasks;
-
