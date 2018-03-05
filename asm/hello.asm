97bec7757d38006598009fa07ce28d21ecdef0c2
Jacob Killelea <Jkillelea@protonmail.ch>
2018-03-04 18:40:25 -0700

    misc formatting

--- asm/hello.asm
+++ asm/hello.asm
@@ -2,9 +2,9 @@
 ; Assemble with nasm -f elf64 hello.nasm && ld hello.o
 ; Written for Linux
 
-section .data     ; read only
-  hello: db "hello world", 10, 0 ; hello is of type char* in C
-  len:   equ $-hello             ; Length of the string. Special NASM syntax.
+section .data    ; read only
+  hello: db  "hello world", 10, 0 ; hello is of type char* in C
+  len:  equ $-hello              ; Length of the string. Special NASM syntax.
 
   n: dq 10       ; Write out the string hello n times. Note that 'n' is a 
                  ; pointer to the actual value. 'dq' means "write quad-word" (8 bytes). 
@@ -21,14 +21,13 @@ _start:
 loop_start:      ; write out "hello" n times
   mov rax, 4     ; write
   mov rbx, 1     ; stdout
-  mov rcx, hello ; the string
+  mov rcx, hello ; pointer to string
   mov rdx, len   ; its length
   int 0x80       ; syscall
 
-  dec rdi        ; i--
-  jnz loop_start ; i > 0 => goto loop_start
+  dec rdi        ; i -= 1
+  jnz loop_start ; i > 0; then goto loop_start
 
-  mov  rbx, 0    ; exit code (no error)
   call exit      ; done
   add  rsp, 8    ; restore the previous stack frame
   pop  rbp       ; ^ these two lines are never reached, they're just here for completenness
@@ -37,7 +36,7 @@ exit:
   push rbp
   mov  rbp, rsp  ; new stack
   sub  rsp, 1    ; add a byte
-  mov  rbx, rax  ; exit code
+  mov  rbx, 0    ; exit code
   mov  rax, 1    ; exit
   int  0x80      ; syscall (doesn't return)
   add  rsp, 1    ; these are here for completness' sake
