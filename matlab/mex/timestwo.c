7c786508b3f68463a895330daca2ed2d6f94ee48
Jacob Killelea <Jkillelea@protonmail.ch>
2018-03-13 14:45:29 -0600

    reorganize

--- matlab/mex/timestwo.c
+++ matlab/mex/timestwo.c
@@ -2,8 +2,8 @@
 #include "mex.h"
 
 /* A quick MEX function. To compile:
- * <matlab> mex timestwo.c
- * <matlab> timestwo(12)
+ * matlab> mex timestwo.c
+ * matlab> timestwo(12)
  * ans =
  *    24
  */
@@ -13,11 +13,11 @@ void mexFunction( int      nlhs,           // number  of returns
                   mxArray *plhs[],         // pointer to returns (might be empty)
                   int      nrhs,           // number  of inputs
                   const mxArray *prhs[]) { // pointer to inputs  (might be empty)
-  double *x, *y;
-  size_t mrows, ncols;
+  double *x, *y; // data arrays
+  size_t mrows, ncols; // size of data
 
   if(nrhs != 1)
-    mexErrMsgTxt("One argument required.");
+    mexErrMsgTxt("One argument required."); // function will return here
 
   mrows = mxGetM(prhs[0]); // number of rows of first arg
   ncols = mxGetN(prhs[0]); // number of cols
@@ -29,7 +29,6 @@ void mexFunction( int      nlhs,           // number  of returns
   // get values
   x = mxGetPr(prhs[0]);
   y = mxGetPr(plhs[0]);
-
 
   for(int i = 0; i < mrows*ncols; i++) {
     y[i] = 2 * x[i];
